a4f7895c1c4e87e68d40ec521bc3e93e357f7941
Abolo Samuel <ikabolo59@gmail.com>
2023-05-11 14:15:11 +0100

    Updated configs

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DATABASE_URI = os.getenv('DATABASE_URI', 'mongodb://localhost:27017/')
 DATABASE_NAME = 'getamanpower'
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
